02c132c0a3cb8eb6a222ef757191fa242b387d1b
AyushTripathiIITP <101452500+AyushTripathiIITP@users.noreply.github.com>
2022-03-29 15:41:50 +0530

    classOn29/03/22
    lecture0

---
+++ demo.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    printf("My First Programme in C\n");
+    printf("Hello, my name is Ayush Tripathi\n");
+    printf("Love you computer programming\n");
+return 0;
+}
